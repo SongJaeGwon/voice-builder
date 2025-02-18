dc240a4b15f5b01601222758f102ea3cefdbacb2
0Chan-smc <cody@thesmc.co.kr>
2025-02-18 16:56:54 +0900

    feat: UI에 시작-종료 시간 추가

--- video_processing/trimmer.py
+++ video_processing/trimmer.py
@@ -1,6 +1,17 @@
 from video_processing import subprocess, get_file_path
 
-def trim_video(input_file, filename="trimmed_video.mp4", start_time="00:00:00", duration="30"):
+def trim_video(input_file, start_time, end_time, filename="trimmed_video.mp4"):
+
+    def time_to_seconds(time_str):
+        h, m, s = map(int, time_str.split(':'))
+        return h * 3600 + m * 60 + s
+
+    start_seconds = time_to_seconds(start_time)
+    end_seconds = time_to_seconds(end_time)
+    duration = end_seconds - start_seconds
+    if not isinstance(duration, (int, float)):
+        raise ValueError("Duration must be a number")
+
     output_file = get_file_path(filename)
     command = f"ffmpeg -i {input_file} -ss {start_time} -t {duration} -c:v copy -c:a copy {output_file} -y"
     subprocess.run(command, shell=True)
